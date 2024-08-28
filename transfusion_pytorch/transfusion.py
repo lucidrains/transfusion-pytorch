0ca0c048584f3f87feb5039368e473263a6a2b31
lucidrains <lucidrains@gmail.com>
2024-08-28 07:28:20 -0700

    classic sota transformer

--- transfusion_pytorch/transfusion.py
+++ transfusion_pytorch/transfusion.py
@@ -17,6 +17,34 @@ def default(v, d):
 
 # attention
 
+class RMSNorm(Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.scale = dim ** 0.5
+        self.gamma = nn.Parameter(torch.zeros(dim))
+
+    def forward(self, x):
+        return F.normalize(x, dim = -1) * self.scale * (self.gamma + 1) # use unit offset from Ohad Rubin
+
+class GEGLU(Module):
+    def forward(self, x):
+        x, gates = x.chunk(2, dim = -1)
+        return F.gelu(gates) * x
+
+def FeedForward(
+    dim,
+    expansion_factor = 4.,
+    dropout = 0.
+):
+    dim_inner = int(dim * expansion_factor * 2 / 3)
+    return nn.Sequential(
+        RMSNorm(dim),
+        nn.Linear(dim, dim_inner * 2),
+        GEGLU(),
+        nn.Dropout(dropout),
+        nn.Linear(dim_inner, dim)
+    )
+
 class Attention(Module):
     def __init__(
         self,
@@ -29,6 +57,8 @@ class Attention(Module):
         super().__init__()
         self.scale = dim_head ** -0.5
         dim_inner = dim_head * heads
+
+        self.norm = RMSNorm(dim)
 
         self.to_qkv = nn.Sequential(
             nn.Linear(dim, dim_inner * 3, bias = False),
@@ -47,19 +77,60 @@ class Attention(Module):
         x,
         attn_mask = None
     ):
+        x = self.norm(x)
+
         q, k, v = self.to_qkv(x)
 
         q = q * self.scale
         sim = einsum(q, k, 'b h i d, b h j d -> b h i j')
 
         if exists(attn_mask):
-            sim = sim.masked_fill(~attn_mask, -torch.finfo(sim.dtype).max)
+            mask_value = -torch.finfo(sim.dtype).max
+            sim = einx.where('b i j, b h i j, -> b h i j', attn_mask, sim, mask_value)
 
         attn = sim.softmax(dim = -1)
+
+        attn = self.dropout(attn)
 
         out = einsum(attn, v, 'b h i j, b h j d -> b h i d')
 
         return self.to_out(out)
+
+class Transformer(Module):
+    def __init__(
+        self,
+        dim,
+        *,
+        depth,
+        dim_head = 64,
+        heads = 8,
+        dropout = 0.,
+        ff_expansion_factor = 4,
+    ):
+        super().__init__()
+
+        layers = ModuleList([])
+
+        for _ in range(depth):
+            layers.append(ModuleList([
+                Attention(dim = dim, dim_head = dim_head, heads = heads, dropout = dropout),
+                FeedForward(dim = dim, expansion_factor = ff_expansion_factor)
+            ]))
+
+        self.layers = layers
+        self.norm = RMSNorm(dim)
+
+    def forward(
+        self,
+        x,
+        attn_mask = None
+    ):
+
+        for attn, ff in self.layers:
+            x = attn(x) + x
+            x = ff(x) + x
+
+        return self.norm(x)
 
 # classes
 
