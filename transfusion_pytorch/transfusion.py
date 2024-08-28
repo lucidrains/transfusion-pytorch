29a79d2d791e959a1b9b9972a9b0159acc2973af
lucidrains <lucidrains@gmail.com>
2024-08-28 06:38:40 -0700

    start with the essential ingredient

--- transfusion_pytorch/transfusion.py
+++ transfusion_pytorch/transfusion.py
@@ -1,6 +1,11 @@
 import torch
+from torch import nn
 import torch.nn.functional as F
 from torch.nn import Module, ModuleList
+
+import einx
+from einops import rearrange, repeat, einsum
+from einops.layers.torch import Rearrange
 
 # helper functions
 
@@ -9,6 +14,52 @@ def exists(v):
 
 def default(v, d):
     return v if exists(v) else d
+
+# attention
+
+class Attention(Module):
+    def __init__(
+        self,
+        dim,
+        dim_head = 64,
+        heads = 8,
+        dropout = 0.,
+
+    ):
+        super().__init__()
+        self.scale = dim_head ** -0.5
+        dim_inner = dim_head * heads
+
+        self.to_qkv = nn.Sequential(
+            nn.Linear(dim, dim_inner * 3, bias = False),
+            Rearrange('b n (qkv h d) -> qkv b h n d', qkv = 3, h = heads)
+        )
+
+        self.dropout = nn.Dropout(dropout)
+
+        self.to_out = nn.Sequential(
+            Rearrange('b h n d -> b n (h d)'),
+            nn.Linear(dim_inner, dim, bias = False)
+        )
+
+    def forward(
+        self,
+        x,
+        attn_mask = None
+    ):
+        q, k, v = self.to_qkv(x)
+
+        q = q * self.scale
+        sim = einsum(q, k, 'b h i d, b h j d -> b h i j')
+
+        if exists(attn_mask):
+            sim = sim.masked_fill(~attn_mask, -torch.finfo(sim.dtype).max)
+
+        attn = sim.softmax(dim = -1)
+
+        out = einsum(attn, v, 'b h i j, b h j d -> b h i d')
+
+        return self.to_out(out)
 
 # classes
 
